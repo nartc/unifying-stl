454f22c346acaf2c0b2e5a9163f11d6d752e9bcd
Chau <ctch5@mail.umsl.edu>
2017-11-04 16:01:29 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,48 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Router, RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { EventsComponent } from './components/events/events.component';
+import { CheckInComponent } from './components/check-in/check-in.component';
+import { MessangerComponent } from './components/messanger/messanger.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { MapComponent } from './components/map/map.component';
+
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'events', component: EventsComponent },
+  { path: 'messages', component: MessangerComponent },
+  { path: 'checkin', component: CheckInComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'map', component: MapComponent }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RegisterComponent,
+    LoginComponent,
+    ProfileComponent,
+    EventsComponent,
+    CheckInComponent,
+    MessangerComponent,
+    NavbarComponent,
+    MapComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/register/register.component.ts
@@ -0,0 +1,22 @@
+import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  constructor(
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+  }
+
+  onSubmitClick() {
+    this.router.navigate(['/login']);
+  }
+
+}
